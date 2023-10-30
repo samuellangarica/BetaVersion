a1701c6baa2e6402cc118d2d9040bb32949ac456
samuellangarica <142830126+samuellangarica@users.noreply.github.com>
2023-10-29 22:37:40 -0600

    setup and pollution api working

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('TEST!');
+  res.render('index');
 });
 
+app.get('/get-temperature', async (req, res) => {
+  const apiKey = '4582102f-35ae-4624-a96d-f28464c3b427';
+  const year = req.query.year;
+  const url = `http://api.airvisual.com/v2/nearest_city?key=${apiKey}`; // GET data for nearest city using IP afdress
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Correct!---------------");
+    console.log(data);
+    res.render('weather-result', { weatherData: data });
+  } catch (error) {
+    res.send('Error retrieveing data!');
+  }
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-no
