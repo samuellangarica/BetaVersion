2fe0aed3bef62af923490478c3e1adf63cb5edcf
samuellangarica <142830126+samuellangarica@users.noreply.github.com>
2023-10-29 19:46:28 -0600

    01

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('TEST!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+no
