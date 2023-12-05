2618e16790926caa60168111a4c6df2f07ba8bf8
fongcito777 <0249352@up.edu.mx>
2023-12-05 16:27:46 -0600

    Pre-mongo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
